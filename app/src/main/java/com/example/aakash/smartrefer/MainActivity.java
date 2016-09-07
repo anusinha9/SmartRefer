acf559ab480c5a365b4a2d26dacdab3ea62cd080
aksstar <aakashg12@gmail.com>
2016-09-07 16:53:28 +0530

    First commit

---
+++ app/src/main/java/com/example/aakash/smartrefer/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.aakash.smartrefer;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final Button button = (Button) findViewById(R.id.button4);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),DoThings.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
