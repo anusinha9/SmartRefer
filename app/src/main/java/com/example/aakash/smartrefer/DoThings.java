a2f95eef1b48d98b28eb02aa08743848b1ae4fa6
aksstar <aakashg12@gmail.com>
2016-10-23 11:39:32 +0530

    bug fixes

--- app/src/main/java/com/example/aakash/smartrefer/DoThings.java
+++ app/src/main/java/com/example/aakash/smartrefer/DoThings.java
@@ -13,21 +13,21 @@ public class DoThings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_do_things);
-        Button x= (Button) findViewById(R.id.button);
+        Button x = (Button) findViewById(R.id.button);
         x.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent=new Intent(getApplicationContext(),Scanactivty.class);
+                Intent intent = new Intent(getApplicationContext(), Scanactivty.class);
                 startActivity(intent);
             }
         });
-        Button y= (Button) findViewById(R.id.button6);
+        Button y = (Button) findViewById(R.id.button6);
         y.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent=new Intent(getApplicationContext(),Dashboard.class);
+                Intent intent = new Intent(getApplicationContext(), Dashboard.class);
                 startActivity(intent);
             }
         });
-        Button z= (Button) findViewById(R.id.button7);
+        Button z = (Button) findViewById(R.id.button7);
         z.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 String url = "http://www.karmayog.org/nonprofits/npogriddisplay.asp?r=476";
@@ -36,33 +36,50 @@ public class DoThings extends AppCompatActivity {
                 startActivity(i);
             }
         });
-        Button zx= (Button) findViewById(R.id.button5);
+        Button zx = (Button) findViewById(R.id.button5);
         zx.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent=new Intent(getApplicationContext(),addthings.class);
+                Intent intent = new Intent(getApplicationContext(), addthings.class);
                 startActivity(intent);
             }
         });
-        Button zxx= (Button) findViewById(R.id.button3);
+        Button zxx = (Button) findViewById(R.id.button3);
         zxx.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent=new Intent(getApplicationContext(),shoppinglist.class);
+                Intent intent = new Intent(getApplicationContext(), shoppinglist.class);
                 startActivity(intent);
             }
         });
-        Button zxxx= (Button) findViewById(R.id.button8);
+        Button zxxx = (Button) findViewById(R.id.button8);
         zxxx.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/results?search_query=healthy+recipes+for+kids")));
             }
         });
-        Button zxxxx= (Button) findViewById(R.id.button2);
+        Button zxxxx = (Button) findViewById(R.id.button2);
         zxxxx.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 String url = "http://myfridgefood.com/";
                 Intent i = new Intent(Intent.ACTION_VIEW);
                 i.setData(Uri.parse(url));
                 startActivity(i);
+            }
+        });
+        Button zxxxxx = (Button) findViewById(R.id.button10);
+        zxxxxx.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String url = "http://www.biogas-india.com/contact.php";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+
+        Button zxxxxxx = (Button) findViewById(R.id.button11);
+        zxxxxxx.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), nutrients.class);
+                startActivity(intent);
             }
         });
     }
